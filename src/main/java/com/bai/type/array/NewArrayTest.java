1d24539364db4fc5ff9f16c3c462db97cd7d3b88
baizhuangzhuang <baizhuangzhuang@bailun.com>
2021-07-27 15:55:22 +0800

    update

---
+++ src/main/java/com/bai/type/array/NewArrayTest.java
@@ -0,0 +1,14 @@
+package com.bai.type.array;
+
+public class NewArrayTest {
+    public static void main(String[] args) {
+
+        int[] arr = new int[10];
+
+        Integer[] arr2 = new Integer[10];
+
+        System.out.println(arr[0]);
+
+        System.out.println(arr2[0]);
+    }
+}
